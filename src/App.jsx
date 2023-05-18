e1600a5efea47cd7cd8503aa6dc256dc917e3574
pc <pc@pcs-MacBook-Pro.local>
2023-05-18 17:48:35 +0400

    finished styling with tilwind

--- src/App.jsx
+++ src/App.jsx
@@ -8,35 +8,24 @@ import dice from "../public/images/icon-dice.svg";
 
 function App() {
   return (
-    <div className="bg-DarkBlue  flex items-center justify-center min-h-screen">
+    <div className="bg-DarkBlue flex items-center justify-center min-h-screen pl-3 pr-3 ">
       <div
-        className="bg-DarkGrayishBlue sm:h-315px sm:w-343px md:h-332px md:w-540px md:rounded-lg"
+        className="bg-DarkGrayishBlue sm:h-auto sm:w-343px md:h-332px md:w-540px md:rounded-lg sm:rounded-lg relative pl-3 pr-3"
         style={{ borderRadius: "10px" }}
       >
-        <h3 className="text-lg font-bold text-NeonGreen pt-12 flex items-center justify-center">
+        <h3 className="text-sm font-light text-NeonGreen pt-12 flex items-center justify-center">
           advice number #
         </h3>
-        <p className="text-lg font-bold text-LightCyan flex items-center justify-center pt-3 pl-3 pr-3 min-w-full max-w-xl">
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-          Necessitatibus adipisci reprehenderit beatae optio possimus magni
-          cupiditate debitis, sit ad distinctio quae facilis vero ducimus
-          obcaecati vel. Dicta perferendis accusamus nostrum. Lorem ipsum dolor
-          sit amet consectetur adipisicing elit. Nam doloribus ipsum officia
-          praesentium provident veritatis nesciunt totam eius placeat tempore
-          libero aperiam rerum beatae, id quis magni dolore debitis voluptates?
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates
-          natus ipsum vitae quidem quibusdam veritatis id deserunt, eaque
-          consequuntur. Repudiandae iure placeat voluptatibus consectetur
-          quibusdam cum dignissimos, praesentium soluta quae!
+        <p className="text-lg font-bold text-LightCyan flex  text-center items-center justify-center pt-3 pl-3 pr-3 min-w-full max-w-xl">
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Lorem ipsum
         </p>
         <div className="flex items-center justify-center pt-6 pb-20">
           <img src={iconMobile} alt="icon" />
         </div>
-        <div
-          className="bg-NeonGreen w-16 h-16 "
-          style={{ borderRadius: "50%" }}
-        >
-          <img className=" m-auto flex p-5" src={dice} alt="dice" />
+        <div className="absolute left-1/2 transform -translate-x-1/2 bottom-0 -mb-8">
+          <div className="dice bg-NeonGreen w-16 h-16 rounded-full flex items-center justify-center">
+            <img className="p-5" src={dice} alt="dice" />
+          </div>
         </div>
       </div>
     </div>
